714da2be11904413dd2fe4b288c8f3bebed487ff
Dan Beccaria <beccdaniel@gmail.com>
2019-07-03 16:09:51 -0400

    added all files and some starting code in the js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+document.ready(function () {
+    var questions = [{
+        quest1: "What is the flight speed of an african swallow?"
+    }]
+    var qDiv = ("<div>" + questions.quest1 + "</div>" )
+$('#mainQuestions').append(qDiv)
+
+
+
+
+
+
+
+})
