a0bcf1550514b3adf23998cff56c14c2dd7abbbe
Dan Beccaria <beccdaniel@gmail.com>
2019-07-06 12:55:57 -0400

    added a submit button user can click before timer runs out: working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,31 +7,45 @@ $(document).ready(function () {
   
 $('h1').css('background', 'red');
 $('p').css('background', 'red');    
-$('#countdown').append('<button id=startGame>Start Game</button>').css('text-align', 'center')
-$('#showresult').hide()
+$('#countdown').append('<button id=startGame>Start Quiz</button>').css('text-align', 'center')
 
 
- 
+
+
 
 $('#startGame').on('click', function(){ 
-    var count = 60
-    
-    getQuest()
+    var count = 15;
    
+    getQuest();
+    $('#showresult').hide();
+    
+
     var interVal = setInterval (function() {
          $('#countdown').text('00:' + --count)
-            
+         
         console.log('countdown works')
         if(count === 0) {
-            $('#mainquestions').hide()
-            $('#showresult').text(checkAnswer()).show().css('border', '1px solid black')
+        $('#mainquestions').hide()
+        
+        showRes();
+        
+          
+           
             clearInterval(interVal);
             console.log("if statement works")
         }   
     }, 1000)
         
-    
-})
+    $('#mainquestions').append('<br><input id=showresultsnow type=submit>')
+         $('#showresultsnow').on('click', function() {
+             $('#mainquestions').hide()
+             showRes()
+             interVal;
+             clearInterval(interVal);
+            
+         })
+            
+});
 
     
 //Storing each question and answer object inside of an array for easier access later on.
@@ -42,14 +56,14 @@ $('#startGame').on('click', function(){
     { 
         q: 'Who directed Mandy?',
         opt: ['Panos Cosmatos', 'Wes Anderson','Martin Scorsese','Tod Solonze'],
-        
+        //Correct 0
     },
        
     {
 
         q:'What is the name of Nicholas Cage\'s Character?',
         opt: ['Arnold Miller', 'Red Miller','John Miller','Fred Miller'],
-        
+        //Correct 1
     },  
        
          
@@ -58,7 +72,7 @@ $('#startGame').on('click', function(){
      
         q:'What is the name of the horn the cultists use to summon the bikers?',
         opt: ['Horn of the Dawn','Horn of the Abyss','Horn of Abraxas','Horn of Darkness'],
-        
+        //Correct 2
         
     },
     
@@ -66,21 +80,32 @@ $('#startGame').on('click', function(){
     {
         q: 'What is the name of the cult?',
         opt: ['Children of the Night','Children of the New Dawn','Children of the Sun','Children of the Corn'],
-        
+        //Correct 1
     },   
        
       {      
         
         q:'What is used to restrain the main character when he is taken by the bikers?',
         opt: ['Rope', 'Duct Tape','Barbed Wire','Extension Cord'],
-        
+        //Correct 2
       }, 
         
      {
         q: 'What planet does Mandy mention is her favorite?',
         opt: ['Saturn', 'Uranus','Pluto','Jupiter'],
-        
-     } 
+        //Correct 3
+     },
+     {
+         q:'What is the name of the leader of the cult?',
+         opt: ['Jeremiah', 'Benjamin','Ezekiel','Malachai']
+         //Correct 0
+     },
+    {
+        q: 'What commercial does the main character see after he escapes his restraints?',
+        opt: ['Coca-cola','Emergency Broadcast','Cheddar Goblin','Kraft Mac & Cheese']
+        //Correct 2
+    }
+
     ]    
            
         
@@ -88,82 +113,188 @@ $('#startGame').on('click', function(){
 
 
 console.log(qA[0].opt)
-console.log(qA[0].answer)
 
 
-console.log(qA[0].opt0)
+
+
 
 
 //Displaying questions and answer options onto the page
 
 function getQuest() {
-       
+      
         for(var i  = 0; i < qA.length; i++) {
             var question = qA[i].q
-            $('#mainquestions').append('<div id=question' + i + '>' + question + '</div>');
+            $('#mainquestions').append('<div id=question' + i + '>' + question + '</div><br>');
             console.log('loop is working');
         }
         for(var a = 0; a < qA[0].opt.length; a++) {
             var answer = qA[0].opt[a];
-            $('#question0').append('<input class=style type=radio name=answer0 value =' + a +'>' + answer);
+            $('#question0').append('<br><input class=style type=radio name=answer0 value =' + a +'>' + answer)
            
         }
         for(var b = 0; b < qA[1].opt.length; b++) {
             var answer1 = qA[1].opt[b];
-            $('#question1').append('<input class=style type=radio name=answer1 value =' + b +'>' + answer1);
+            $('#question1').append('<br><input class=style type=radio name=answer1 value =' + b +'>' + answer1);
         }
         for(var c = 0; c < qA[2].opt.length; c++) {
             var answer2 = qA[2].opt[c];
-            $('#question2').append('<input class=style type=radio name=answer2 value =' + c +'>' + answer2);
+            $('#question2').append('<br><input class=style type=radio name=answer2 value =' + c +'>' + answer2);
         }
         for(var d = 0; d < qA[3].opt.length; d++) {
             var answer3 = qA[3].opt[d];
-            $('#question3').append('<input class=style type=radio name=answer3 value ='+ d +'>' + answer3);
+            $('#question3').append('<br><input class=style type=radio name=answer3 value ='+ d +'>' + answer3);
         }
         for(var e = 0; e < qA[4].opt.length;e++) {
             var answer4 = qA[4].opt[e];
-            $('#question4').append('<input class=style type=radio name=answer4 value =' + e + '>' + answer4);
+            $('#question4').append('<br><input class=style type=radio name=answer4 value =' + e + '>' + answer4);
         }
         for(var f = 0; f < qA[5].opt.length; f++) {    
             var answer5 = qA[5].opt[f];
-            $('#question5').append('<input class=style type=radio name=answer5 value =' + f +'>' + answer5);
+            $('#question5').append('<br><input class=style type=radio name=answer5 value =' + f +'>' + answer5);
         } 
-       showRes()
+        for(var g = 0; g < qA[6].opt.length; g++) {
+            var answer6 = qA[6].opt[g];
+            $('#question6').append('<br><input class=style type=radio name answer6 value=' + g + '>' + answer6)
+        }
+        for(var h = 0; h < qA[7].opt.length; h++) {
+            var answer7 = qA[7].opt[h];
+            $('#question7').append('<br><input class=style type=radio name answer7 value=' + h + '>' + answer7)
+        }
+       
       
 }
         
 //Checking if user selected the right answer/no answer at all/ or the wrong answer  
 
-function checkAnswer() {
+function showRes() {
  // grab the input elements in each question console.log($(#question0 input)); 
+    unanswered = 0;
+    incorrect = 0;
+    correct = 0;
+    
+    
+    const answers = [0,1,2,3];
+  // Spread operator to convert input values into an array
+    let getAnswer = [...$('#question0 input')];
+    let getAnswer1 = [...$('#question1 input')];
+    let getAnswer2 = [...$('#question2 input')];
+    let getAnswer3 = [...$('#question3 input')];
+    let getAnswer4 = [...$('#question4 input')];
+    let getAnswer5 = [...$('#question5 input')];
+    let getAnswer6 = [...$('#question6 input')];
+    let getAnswer7 = [...$('#question7 input')];
 
-
-    correct = 0
-  // console.log($('#question0 input'));
-    let grabbedAnswers = [...$('#question0 input')];
-// loop thru the grabbed inputs and see which one is checked
-    console.log(grabbedAnswers)
-    const correctAnswers = [0,1,2,3]
-// if the the chosenAnswer  is the same as the right answer 
-       
-    let chosenAnswer = grabbedAnswers.find(function(x) {
+   
+    
+        
+    let userAnswer = getAnswer.find(function(x) {
+        return x.checked === true;
+    })
+    
+    let userAnswer1 = getAnswer1.find(function(x) {
+        return x.checked === true;
+    })
+    let userAnswer2 = getAnswer2.find(function(x) {
+        return x.checked === true;
+    })  
+    let userAnswer3 = getAnswer3.find(function(x) {
+        return x.checked === true;
+    })
+    let userAnswer4 = getAnswer4.find(function(x) {
+        return x.checked === true;
+    })
+    let userAnswer5 = getAnswer5.find(function(x) {
         return x.checked === true;
     })
 
-    let chosenValue = $(chosenAnswer).val()
+    let userAnswer6 = getAnswer6.find(function(x){
+        return x.checked === true;
+    })
+    let userAnswer7 = getAnswer7.find(function(x) {
+        return x.checked === true;
+    })
 
-    if (chosenValue === correctAnswers[0]) {
+    let chosenValue = $(userAnswer).val()
+    let chosenValue1 = $(userAnswer1).val()
+    let chosenValue2 = $(userAnswer2).val()
+    let chosenValue3 = $(userAnswer3).val()
+    let chosenValue4 = $(userAnswer4).val()
+    let chosenValue5 = $(userAnswer5).val()
+    let chosenValue6 = $(userAnswer6).val()
+    let chosenValue7 = $(userAnswer7).val()
+    //------q1
+    if (chosenValue == answers[0] || userAnswer === true) {
         correct++;
+        $('#showresult').text('You got: ' + correct + ' right').show()
+      
+        console.log('check answer if statement works')
+    } else {
+            incorrect++
+            $('#showresult').text('You got ' + incorrect + ' wrong').show()
     }
-    
+    /*
+        console.log(chosenValue)
+    //-------q2
+    if(chosenValue1 == answers[1]) {
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    //-------q3
+    if(chosenValue2 == answers[2]) {
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    //--------q4
+    if(chosenValue3 == answers[1]) {
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    //--------q5
+    if(chosenValue4 == answers[2]) {
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    //----------q6
+    if(chosenValue5 == answers[3]) {
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    //----------q7
+    if(chosenValue6 == answers[0]){
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    //-----------q8
+    if(chosenValue7 == answers[2]) {
+        correct++
+        $('#showresult').text('You got ' + correct + ' right').show()
+    } else {
+        incorrect++
+        $('#showresult').text('You got ' + incorrect + ' wrong').show()
+    }
+    */
+}       
 
-     // increment right answers
 
-    
-   
-   // debugger;
-    
-  
 
     
        
@@ -172,15 +303,7 @@ function checkAnswer() {
     
     
     
-}
 
-function showRes() {
-    $('#mainquestions').append('<button id=showbutton>Submit</button>')
-    $('#showbutton').on('click', function() {
-         $('#mainquestions').hide()
-         $('#showresult').text(checkAnswer()).css('border', '1px solid black')
-    })
-}
 
 
 
