82fd8e3537082e75672aafc63d6d2a644039c101
Dan Beccaria <beccdaniel@gmail.com>
2019-07-08 16:35:00 -0400

    found another way to track user answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 $('h1').css('background', 'red');
 $('p').css('background', 'red');    
 $('#countdown').append('<button id=startGame>Start Quiz</button>').css('text-align', 'center')
-$('#showresult').append('<div id=restart>Restart</div>')
+$('#showresult').append('<button id=restart>Restart</button>')
 $('#restart').hide()
 $('#restart').on('click', function() {
     location.reload();
@@ -24,6 +24,8 @@ $('#startGame').on('click', function(){
          $('#countdown').text('00:' + --count)
         if(count === 0) {
         $('#mainquestions').hide()
+        $('#restart').show()
+        $('#showresult').show()
         showRes();
         clearInterval(countDown);
         }   
@@ -32,7 +34,8 @@ $('#startGame').on('click', function(){
          $('#showresultsnow').on('click', function() {
              $('#mainquestions').hide()
              showRes()
-             $('#restart').show()
+            $('#restart').show()
+            $('#showresult').show()
              countDown;
              clearInterval(countDown);
             
@@ -43,46 +46,46 @@ $('#startGame').on('click', function(){
     { 
         q: 'Who directed Mandy?',
         opt: ['Panos Cosmatos', 'Wes Anderson','Martin Scorsese','Tod Solonze'],
-        right: 0
+        
     },
     {
 
         q:'What is the name of Nicholas Cage\'s Character?',
         opt: ['Arnold Miller', 'Red Miller','John Miller','Fred Miller'],
-        right: 1
+       
     }, 
     {
      
         q:'What is the name of the horn the cultists use to summon the bikers?',
         opt: ['Horn of the Dawn','Horn of the Abyss','Horn of Abraxas','Horn of Darkness'],
-        right: 2
+        
         
     },
     {
         q: 'What is the name of the cult?',
         opt: ['Children of the Night','Children of the New Dawn','Children of the Sun','Children of the Corn'],
-        right: 1
+        
     },   
     {      
         
         q:'What is used to restrain the main character when he is taken by the bikers?',
         opt: ['Rope', 'Duct Tape','Barbed Wire','Extension Cord'],
-        right: 2
+        
     }, 
     {
         q: 'What planet does Mandy mention is her favorite?',
         opt: ['Saturn', 'Uranus','Pluto','Jupiter'],
-        right: 3
+        
      },
      {
          q:'What is the name of the leader of the cult?',
          opt: ['Jeremiah', 'Benjamin','Ezekiel','Malachai'],
-         right: 0
+         
      },
     {
         q: 'What commercial does the main character see after he escapes his restraints?',
         opt: ['Coca-cola','Emergency Broadcast','Cheddar Goblin','Kraft Mac & Cheese'],
-        right: 2
+       
     }
 ]    
 //Displaying questions and answer options onto the page
@@ -96,11 +99,11 @@ function getQuest() {
         for(let a = 0; a < qA[i].opt.length; a++) {
                 getAnswer = qA[i].opt[a];
                 
-                $('#question' + i).append('<br> <input class=user type=radio name=answer' + i + ' value=' + a +'>' + getAnswer)
+                $('#question' + i).append('<br> <input class=user' + i +  ' type=radio name=answer' + i + ' value=' + a +'>' + getAnswer)
         }
     }
-showRes()
-    
+
+    showRes()
 }
         
 //Checking if user selected the right answer/no answer at all/ or the wrong answer  
@@ -110,15 +113,49 @@ showRes()
 function showRes() {
     
  // grab the input elements in each question console.log($(#question0 input)); 
-    unanswered = 0;
+    noanswer = 0;
     incorrect = 0;
     correct = 0;
-    
+    let answers = [0,1,2,1,2,3,0,2];
     //Matching values stored in this array to values of the input converted to an array.
-    $('.user').on('click', function() { 
-        let userClick = parseInt($('input').attr('value'))
-        console.log(userClick)
-    })
+    $('.user0').on('click', function() { 
+        answers;
+        let userClick = parseInt($(this).attr('value'))
+        if(userClick === answers[0]) {
+            console.log('first if works')
+            correct++
+            $('#correct').text(correct)
+        } else if(userClick !== answers[0]) {
+            console.log('1st else if works')
+            incorrect++
+            $('#incorrect').text(incorrect)
+        } 
+    })   
+
+    $('.user1').on('click', function() { 
+        answers;
+        let userClick = parseInt($(this).attr('value'))
+        if(userClick === answers[1]) {
+            console.log('2nd if works')
+            correct++
+            $('#correct').text(correct)
+        } else if(userClick !== answers[1]) {
+            console.log('2nd else if works')
+            incorrect++
+            $('#incorrect').text(incorrect)
+        } 
+    }) 
+           
+       
+       
+        
+       
+       
+       
+         
     
-};       
+   
+};  
+
+ 
 }); 
